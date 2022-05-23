1a7604f8530805082f1fb29450fceb0368282eba
rioobod <akanbilola@gmail.com>
2022-05-22 21:30:49 -0500

    fixed error with resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,14 @@
 const { gql } = require("apollo-server-express");
 const typeDefs = gql`
-  type Query {
+
+type Query {
     me: User
+    users: [User]
+    user(username: String!): User
   }
+
   type Mutation {
-    login(email: String!, password: String!): Auth
+    login(email: String, password: String): Auth
     addUser(username: String, email: String, password: String): Auth
     saveBook(input: saveBookInput): User
   }
@@ -18,13 +22,13 @@ const typeDefs = gql`
   }
   type User {
     _id: ID
-    username: String!
-    email: String!
+    username: String
+    email: String
     bookCount: Int
     savedBooks: [Book]
   }
   type Book {
-    authors: String
+    authors: [String]
     description: String
     title: String
     bookId: String
